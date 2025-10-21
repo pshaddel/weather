744eafa558e1594e1b047b0b1375bc7c0cbcbb34
Poorshad <poorshad.Shaddel@redbull.com>
2025-10-21 11:29:13 +0200

    Refactor layout structure in RootLayout and Frame components for improved readability and consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,22 +17,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <main className="flex flex-col items-center justify-between p-36 mt-20 ">
-          <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-            <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200  pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-              <Image
-                src="/logo.png"
-                alt="Red Bull Logo"
-                width={300}
-                height={90}
-                priority
-                style={{ padding: "20px" }}
-              />
-            </p>
-            <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-            </div>
-          </div>
+      <body className={inter.className} style={{ backgroundColor: 'rgba(241,246,246,255)' }}>
+        <div className="fixed left-0 top-0 w-full flex justify-center z-10 pt-4">
+          <Image
+            src="/logo.png"
+            alt="Red Bull Logo"
+            width={300}
+            height={90}
+            priority
+            style={{ padding: "20px" }}
+          />
+        </div>
+        <main className="flex flex-col items-center justify-between p-36 mt-20">
           {children}
         </main>
       </body>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,17 +11,15 @@ export default MyApp;
 export function Frame({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <main className="flex flex-col items-center justify-between p-36 mt-20" style={{ backgroundColor: 'rgba(241,246,246,255)' }}>
-            <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-                <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200  pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-                    <Image
-                        src="/logo.png"
-                        alt="Red Bull Logo"
-                        width={300}
-                        height={90}
-                        priority
-                        style={{ padding: "20px" }}
-                    />
-                </p>
+            <div className="fixed left-0 top-0 w-full flex justify-center z-10 pt-4">
+                <Image
+                    src="/logo.png"
+                    alt="Red Bull Logo"
+                    width={300}
+                    height={90}
+                    priority
+                    style={{ padding: "20px" }}
+                />
             </div>
             {children}
         </main>
